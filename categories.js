90d3995cfd9f48ba619d1d2c4472e42f1467b7e8
annikab12 <annikabiwer@gmx.de>
2024-07-05 11:19:09 +0200

    Update categories.js

--- categories.js
+++ categories.js
@@ -15,7 +15,7 @@ const categories = [
             { text: "Mich interessiert es, wie und warum Menschen Kaufentscheidungen treffen und bin bereit, mich damit auseinanderzusetzen.", weight: 1.25, inverted: false },
             { text: "Ich interessiere mich für die grundlegenden Mechanismen der menschlichen Wahrnehmung und Informationsverarbeitung und bin bereit, mich damit auseinanderzusetzen.", weight: 1.25, inverted: false },
             { text: "Ich interessiere mich für die Prozesse des menschlichen Lernens, Denkens und Problemlösens und bin bereit, mich damit auseinanderzusetzen.", weight: 1.25, inverted: false },
-            { text: "Ich interessiere mich besonders  für die  klinische Psychologie und Psychotherapie und bin bereit mich damit auseinanderzusetzen.", weight: 1, inverted: true }
+            { text: "Ich interessiere mich vor allem  für den Teilbereich klinischen Psychologie und Psychotherapie und bin bereit mich damit auseinanderzusetzen.", weight: 1, inverted: true }
         ]
     },
     {
@@ -30,10 +30,10 @@ const categories = [
     {
         name: "Interdisziplinarität",
         questions: [
-            { text: "Es ist mir wichtig, mich in einem bestimmten Bereich zu vertiefen.", weight: 1.33, inverted: true },
+            { text: "Es ist mir wichtig, mich in einem spezifischen Teilgebiet zu vertiefen.", weight: 1.33, inverted: true },
             { text: "Ich möchte gerne einen Überblick über verschiedene Themengebiete bekommen und die Grundlagen unterschiedlicher Bereiche erlernen.", weight: 1.33, inverted: false },
             { text: "Ich bevorzuge es, eine klare Vorstellung von meinem zukünftigen Karriereweg zu haben, anstatt mir verschiedene Möglichkeiten offen zu halten.", weight: 1.33, inverted: true },
-            { text: "Ich bin in der Lage dazu, Wissen aus verschiedenen Disziplinen miteinander zu verknüpfen und Zusammenhänge zu erkennen.", weight: 2, inverted: false }
+            { text: "Ich habe Interesse daran, Wissen aus verschiedenen Disziplinen miteinander zu verknüpfen und Zusammenhänge zu erkennen.", weight: 2, inverted: false }
         ]
     },
     {
@@ -50,21 +50,21 @@ const categories = [
     {
         name: "Empirisches Arbeiten",
         questions: [
-            { text: "Ich bin interessiert an der Planung, Durchführung und Auswertung empirischer Studien", weight: 1, inverted: false },
+            { text: "Ich bin interessiert an der Planung, Durchführung und Auswertung empirischer Studien.", weight: 1, inverted: false },
             { text: "Ich bin offen dafür, mir Kenntnisse der Statistik und Forschungsmethoden anzueignen.", weight: 2, inverted: false },
-            { text: "Ich bin bereit methodisch Daten zu erheben, zu analysieren und die Ergebnisse strukturiert aufzuarbeiten.", weight: 1, inverted: false },
-            { text: "Ich interessiere mich dafür, wissenschaftliche zu arbeiten und theoretische Hintergründe zu einem bestimmten Thema zu recharchieren.", weight: 2, inverted: false }
+            { text: "Ich bin bereit, methodisch Daten zu erheben, zu analysieren und die Ergebnisse strukturiert aufzuarbeiten.", weight: 1, inverted: false },
+            { text: "Ich bin bereit, wissenschaftlich zu arbeiten und theoretische Hintergründe zu einem bestimmten Thema zu recharchieren.", weight: 2, inverted: false }
         ]
     },
     {
         name: "Berufsvorstellung",
         questions: [
-            { text: "Ich interessiere mich für die Gestaltung und Entwicklung von benutzerfreundlichen und ansprechenden Webseiten, Online Tools, Apps oder Spielen für verschiedene Plattformen.", weight: 1, inverted: false },
-            { text: "Ich interessiere mich für die Analyse und Interpretation von Kundendaten (Consumer research) und deren Nutzung durch Werbeagenturen, um z.B Marketingstrategien zu entwickeln", weight: 1, inverted: false },
-            { text: "Ich interessiere mich für die Nutzung von sozialen Medien und die Planung und Erstellung von Beiträgen, um die Social Media Präsenz eines Unternehmens nach vorne zu treiben.", weight: 1, inverted: false },
-            { text: "Ich interessiere mich für Berufe, die darauf abzielen, Technik für Benutzer verständlich und zugänglich zu machen, sowie für die Unterstützung bei der Umsetzung von IT-Projekten (z.B Usability-Engineers und IT-Consultants)", weight: 1, inverted: false },
-            { text: "Ich kann mir vorstellen Projekte zu leiten und zu koordinieren, sowie die interne und externe Kommunikation in einem Unternehmen zu verbessern.", weight: 1, inverted: false },
-            { text: "Ich interessiere mich dafür, wie Unternehmen soziale Medien und Online Tools nutzen, um Personal zu bewerben und zu gewinnen (z.B Human Resources).", weight: 1, inverted: false }
+            { text: "Ich kann mir vorstellen, später an der Gestaltung und Entwicklung von benutzerfreundlichen und ansprechenden Webseiten, Online Tools, Apps oder Spielen für verschiedene Plattformen zu arbeiten", weight: 1, inverted: false },
+            { text: "Ich kann mir vorstellen, später Kundendaten zu analysieren und zu interpretieren (Consumer research), um daraus z.B Marketingstrategien zu entwickeln.", weight: 1, inverted: false },
+            { text: "Ich kann mir vorstellen, im Social Media Bereich eines Unternehmens zu arbeiten, Beiträge zu planen und zu erstellen, um dessen Social Media Präsenz nach vorne zu treiben.", weight: 1, inverted: false },
+            { text: "Ich kann mir vorstellen, in einem Beruf zu arbeiten, der Technik für Benutzer verständlich und zugänglich macht, sowie bei der Umsetzung von IT-Projekten zu unterstützen (z.B Usability-Engineers und IT-Consultants).", weight: 1, inverted: false },
+            { text: "Ich kann mir vorstellen, Projekte zu leiten und zu koordinieren, sowie die interne und externe Kommunikation in einem Unternehmen zu verbessern.", weight: 1, inverted: false },
+            { text: "Ich kann mir vorstellen, für Unternehmen mithilfe sozialer Medien und Online Tools Personal zu bewerben und zu gewinnen (z.B Human Resources).", weight: 1, inverted: false }
         ]
     },
 ];
